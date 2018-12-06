1c6e69263b4b0728dc4814a7b4cc0f9a30a40fca
Johan Lundberg <lundberg@sunet.se>
2018-12-06 13:43:48 +0100

    apt needs some convincing

--- s3_dep.sh
+++ s3_dep.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 set -e
-apt-get install -y php-curl
+apt-get update
+apt-get install -y -o Dpkg::Options::=--force-confnew php-curl
 cd /opt/filesender/optional-dependencies/s3
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
